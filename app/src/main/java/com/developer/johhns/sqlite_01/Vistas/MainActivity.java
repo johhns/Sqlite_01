28be7ccbddaf778e51e0a1ec2f18f41d1db4899c
johhns <johhns@hotmail.com>
2021-10-12 20:50:16 -0600

    v 1.009

--- app/src/main/java/com/developer/johhns/sqlite_01/Vistas/MainActivity.java
+++ app/src/main/java/com/developer/johhns/sqlite_01/Vistas/MainActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.developer.johhns.sqlite_01.Datos.BaseDatos;
+import com.developer.johhns.sqlite_01.Datos.DB_DML;
 import com.developer.johhns.sqlite_01.Modelos.Sincronizar;
 import com.developer.johhns.sqlite_01.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnDeptos, btnMunicipios , btnNuevoDepto, btnSincronizar ;
+    Button btnDeptos, btnMunicipios , btnNuevoDepto, btnSincronizar , btnEliminarDeptos ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Sincronizar sincronizar = new Sincronizar( view.getContext() ) ;
                 sincronizar.sincronizarDeptos();
+            }
+        });
+
+        btnEliminarDeptos = findViewById( R.id.btnEliminarDeptos ) ;
+        btnEliminarDeptos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DB_DML dml = new DB_DML(view.getContext()) ;
+                dml.eliminarDepartamentos();
             }
         });
 
